2a9f089132b889419eebafd26c5bfce3469c721a
damarma <dmartimarti@gmail.com>
2021-04-22 18:03:52 +0100

    Update unicycler_test.pbs.bat

--- unicycler_test.pbs.bat
+++ unicycler_test.pbs.bat
@@ -1,19 +1,21 @@
 #PBS -l walltime=24:0:0
-#PBS -l select=1:ncpus=32:mem=50gb
+#PBS -l select=1:ncpus=32:mem=30gb
 
 # Load modules for any applications
 
 module load anaconda3/personal
+source activate assembly
 
-WORK=$HOME/Pangenomic_genomes/sequencing/raw/210401_NB501045_0331_AHYK7HBGXH/test/trimm
-
+WORK=$HOME/Pangenomic_genomes/sequencing/raw/210401_NB501045_0331_AHYK7HBGXH/test/trimm_seqs_minlen130
+mkdir $WORK/unicycler_batch_example
 
 for R1paired in $WORK/*R1_001_paired*
 do   	
 	R2paired=${R1paired//R1_001_paired.fastq.gz/R2_001_paired.fastq.gz}
 	R1unpaired=${R1paired//R1_001_paired.fastq.gz/R1_001_unpaired.fastq.gz}   	
 	R2unpaired=${R1paired//R1_001_paired.fastq.gz/R2_001_unpaired.fastq.gz}
-	OUTPUT=${R1paired: -32:-24}
+	OUTLONG=${R1paired##*/}
+    OUTPUT=${OUTLONG::-23}
 
     cp $R1paired $TMPDIR
     cp $R2paired $TMPDIR
@@ -24,6 +26,6 @@ do
 	-s $R1unpaired -s $R2unpaired \
 	-o $OUTPUT -t 32 --no_correct
 
-	cp -r $OUTPUT $WORK/unicycler_batch_example
+	cp -r $OUTPUT $WORK/unicycler_batch_example/
 
 done
