8088895363c9c7a364d33955e61330c3df8a2f73
damarma <dmartimarti@gmail.com>
2021-04-22 18:04:12 +0100

    Create unicycler_batch1.pbs.bat
    job file for batches of 48 genome assemblies

---
+++ unicycler_batch1.pbs.bat
@@ -0,0 +1,31 @@
+#PBS -l walltime=72:0:0
+#PBS -l select=1:ncpus=32:mem=30gb
+
+# Load modules for any applications
+
+module load anaconda3/personal
+source activate assembly
+
+WORK=$HOME/Pangenomic_genomes/sequencing/raw/210401_NB501045_0331_AHYK7HBGXH/Unaligned/trimm_seqs/P1/batch1
+mkdir $WORK/unicycler_batch
+
+for R1paired in $WORK/*R1_001_paired*
+do   	
+	R2paired=${R1paired//R1_001_paired.fastq.gz/R2_001_paired.fastq.gz}
+	R1unpaired=${R1paired//R1_001_paired.fastq.gz/R1_001_unpaired.fastq.gz}   	
+	R2unpaired=${R1paired//R1_001_paired.fastq.gz/R2_001_unpaired.fastq.gz}
+	OUTLONG=${R1paired##*/}
+    OUTPUT=${OUTLONG::-23}
+
+    cp $R1paired $TMPDIR
+    cp $R2paired $TMPDIR
+    cp $R1unpaired $TMPDIR
+    cp $R2unpaired $TMPDIR
+
+    unicycler -1 $R1paired -2 $R2paired \
+	-s $R1unpaired -s $R2unpaired \
+	-o $OUTPUT -t 32 --no_correct
+
+	cp -r $OUTPUT $WORK/unicycler_batch/
+
+done
