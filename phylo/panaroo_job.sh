dc5d135c1ab690df2a7f30d1e53c4da001a9f416
damarma <dmartimarti@gmail.com>
2021-05-05 13:17:18 +0100

    Update panaroo_job.sh
    fix mem and conda env

--- phylo/panaroo_job.sh
+++ phylo/panaroo_job.sh
@@ -1,13 +1,12 @@
 #PBS -l walltime=48:0:0
-#PBS -l select=1:ncpus=32:mem=50gb
+#PBS -l select=1:ncpus=32:mem=24gb
 
 # Load modules for any applications
 
 module load anaconda3/personal
-source activate assembly
+source activate panaroo
 
 WORK=$HOME/pangenome_study/annotations/good_quality
-mkdir $WORK/annot_prokka_v1
 
 cp $WORK/*.gff $TMPDIR
 
