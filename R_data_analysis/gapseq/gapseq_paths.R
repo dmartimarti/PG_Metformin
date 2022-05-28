b5fdcd91b6a387598d4de6a28e4cafe9bd577f43
damarma <dmartimarti@gmail.com>
2022-05-28 16:57:18 +0100

    Update gapseq_paths.R
    new heatmaps for the entire collection

--- R_data_analysis/gapseq/gapseq_paths.R
+++ R_data_analysis/gapseq/gapseq_paths.R
@@ -8,9 +8,15 @@ library(readr)
 library(cowplot)
 library(here)
 library(ComplexHeatmap)
+library(circlize)
 
 
 theme_set(theme_cowplot(15))
+
+
+
+# load data ---------------------------------------------------------------
+
 
 
 # get the genome names
@@ -33,11 +39,43 @@ for (i in 1:length(gnm_names)){
 }
 
 
+# load metadata 
+
+library(readxl)
+metadata = read_excel("~/Documents/MRC_postdoc/Pangenomic/metadata/MAIN_metadata.xlsx")
+
+
+# how many genomes are we lacking ? 
+
+# filter metadata for the bugs we want to analyse: AUS and ECOREF
+meta_filt = metadata %>% 
+  mutate(Genome = str_sub(fasta, start = 1, end = -7), .before = ID) %>% 
+  filter(Discard == 'No') %>% 
+  filter(Origin %in% c('AUS', 'ECOREF')) %>% 
+  distinct(Genome, .keep_all = T) 
+
+
+# save the missing genome list in a csv file
+meta_filt %>% 
+  filter(!(Genome %in% gnm_names)) %>% 
+  write_csv('../missing_genomes.csv')
+  
+
+
+
+
 
 # exploration plots -------------------------------------------------------
 
+# first filter the genome list we want to analyse
+
+genome_paths = genome_paths %>% filter(Genome %in% meta_filt$Genome)
+
 
 # calculate how many pathways per genome are in our dataset
+
+
+## histograms ####
 
 genome_paths %>% 
   filter(Completeness == 100) %>% 
@@ -48,13 +86,29 @@ genome_paths %>%
   ggplot(aes(n)) +
   geom_histogram(color = 'black', fill = 'grey50', bins = 30) +
   labs(
-    x = 'Number of pathways in genome',
+    x = 'Number of complete pathways in genome',
     y = 'Number of genomes'
   )
 
 ggsave("../exploration/number_of_paths.pdf", height = 7, width = 9)
 
 
+genome_paths %>% 
+  filter(Completeness > 0) %>% 
+  select(Name, Genome) %>% 
+  group_by(Genome) %>% 
+  count() %>% 
+  filter(n > 400) %>%
+  ggplot(aes(n)) +
+  geom_histogram(color = 'black', fill = 'grey50', bins = 30) +
+  labs(
+    x = 'Number of pathways in genome',
+    y = 'Number of genomes',
+    caption = 'Pathways not complete (<100% of coverage) are also included in this plot'
+  )
+ggsave("../exploration/number_of_paths_all.pdf", height = 7, width = 9)
+
+## complete paths matrix ####
 paths_pa = genome_paths %>% 
   filter(Completeness == 100) %>% 
   select(Name, Genome) %>% 
@@ -71,12 +125,103 @@ paths_matrix = paths_pa %>%
 rownames(paths_matrix) = gnm_names
 
 
-library(circlize)
-col_fun = colorRamp2(c(-2, 0, 2), c("green", "white", "red"))
+col_fun = colorRamp2(c(0, 1), c("white", "#0949AB"))
 col_fun(seq(-3, 3))
-Heatmap(paths_matrix, col = col_fun)
+Heatmap(paths_matrix, 
+        col = col_fun,
+        name = "Completeness")
 
 
 quartz.save(file = '../exploration/Pathways_heatmap.pdf',
             type = 'pdf', height = 70, width = 90)
+
+
+Heatmap(paths_matrix, 
+        col = col_fun,
+        name = "Completeness",
+        show_row_names = FALSE,
+        show_column_names = FALSE)
+
+quartz.save(file = '../exploration/Pathways_heatmap_noNames.pdf',
+            type = 'pdf', height = 9, width = 12)
+
+
+## difference paths matrix  ####
+
+paths_pa = genome_paths %>% 
+  filter(Completeness == 100) %>% 
+  select(Name, Genome) %>% 
+  mutate(presence = 1) %>% 
+  pivot_wider(names_from = Name, values_from = presence, values_fill = 0)
+
+pahts_names = names(paths_pa)
+gnm_names = paths_pa$Genome
+
+paths_matrix = paths_pa %>% 
+  select(-Genome) %>% 
+  as.matrix() 
+
+rownames(paths_matrix) = gnm_names
+
+# total genomes
+tot_gnms = dim(paths_matrix)[1]
+
+# filter the paths that are NOT 100% complete in at least 99% of genomes
+# (core pathway)
+redux_matrix = paths_matrix[,colSums(paths_matrix) < tot_gnms*0.99]
+dim(redux_matrix)
+
+
+col_fun = colorRamp2(c(0, 1), c("white", "#0949AB"))
+Heatmap(redux_matrix, 
+        col = col_fun,
+        name = "Completeness")
+
+quartz.save(file = '../exploration/Pathways_heatmap_differential.pdf',
+            type = 'pdf', height = 50, width = 70)
+
+Heatmap(redux_matrix, 
+        col = col_fun,
+        name = "Completeness",
+        show_row_names = FALSE,
+        show_column_names = FALSE)
+
+quartz.save(file = '../exploration/Pathways_heatmap_differential_noNames.pdf',
+            type = 'pdf', height = 6, width = 9)
+
+
+
+## all paths matrix ####
+
+paths_pa = genome_paths %>% 
+  filter(Completeness > 0) %>% 
+  mutate(presence = Completeness/100) %>% 
+  select(Name, Genome, presence) %>% 
+  # mutate(presence = 1) %>% 
+  pivot_wider(names_from = Name, values_from = presence, values_fill = 0)
+
+pahts_names = names(paths_pa)
+gnm_names = paths_pa$Genome
+
+paths_matrix = paths_pa %>% 
+  select(-Genome) %>% 
+  as.matrix() 
+
+rownames(paths_matrix) = gnm_names
+
+Heatmap(paths_matrix, 
+        col = col_fun,
+        name = "Completeness")
+
+quartz.save(file = '../exploration/ALL_Pathways_heatmap.pdf',
+            type = 'pdf', height = 70, width = 190)
+
+Heatmap(paths_matrix, 
+        col = col_fun,
+        name = "Completeness",
+        show_row_names = FALSE,
+        show_column_names = FALSE)
+
+quartz.save(file = '../exploration/ALL_Pathways_heatmap_noNames.pdf',
+            type = 'pdf', height = 9, width = 12)
 
