ba0881c11c7413711513bfd688c3c6faba7150da
dmartimarti <dmartimarti@gmail.com>
2022-11-23 09:32:12 +0000

    Update phylo_tree_ALL.R

--- R_data_analysis/phylo_tree_ALL.R
+++ R_data_analysis/phylo_tree_ALL.R
@@ -1,4 +1,3 @@
-
 # libraries ####
 
 library(tidyverse)
@@ -24,19 +23,24 @@ theme_set(theme_light())
 
 # load straindb
 # metadata
-metadata = read_excel("D:/MRC_Postdoc/Pangenomic/pangenome_analysis/metadata/MAIN_metadata.xlsx", 
-                      sheet = "metadata")
+# metadata = read_excel("D:/MRC_Postdoc/Pangenomic/pangenome_analysis/metadata/MAIN_metadata.xlsx", 
+#                       sheet = "metadata")
+# metadata <- read_excel("data/MAIN_metadata.xlsx")
+
+metadata = read_excel("data/Metadata_phenotype_extended.xlsx")
 
 
 # load tree
-tree = read.tree('D:/MRC_Postdoc/Pangenomic/pangenome_analysis/ALL/phylo_analysis/panaroo_results_noEVO/core_fast_tree.treefile')
-
+# tree = read.tree('D:/MRC_Postdoc/Pangenomic/pangenome_analysis/ALL/phylo_analysis/panaroo_results_noEVO/core_fast_tree.treefile')
+tree = read.tree('data/core_fast_tree.treefile')
 
 ## WORM
 # summary statistics from worm imaging
 
-worm.img =  read_excel("D:/MRC_Postdoc/Pangenomic/pangenome_analysis/ALL/worm_imaging/ALL_worm_stats.xlsx", 
+worm.img =  read_excel("data/ALL_worm_stats.xlsx", 
                        sheet = "Stats_per_strain")
+
+
 
 ## BACTERIA
 
@@ -87,6 +91,42 @@ grp = list(
 )
 
 tree = tree %>% groupOTU(grp, 'phylogroup')
+
+
+tree_phylogroup = function(tree) {
+  
+  # phylogroup info
+  sp_names = c(tree$tip.label)
+  df = data.frame(sp_names)
+  colnames(df) = 'label'
+  
+  # add phylogroup info and fix NT12139 strain
+  df = df %>% left_join(metadata %>% 
+                          select(names, phylogroup) %>%
+                          rename(label = names)) 
+  unique(df$phylogroup)
+  
+  
+  # root tree and specify groups 
+  grp = list(
+    'A' = df %>% filter(phylogroup == 'A') %>% pull(label),
+    'B1' = df %>% filter(phylogroup == 'B1') %>% pull(label),
+    'B2' = df %>% filter(phylogroup == 'B2') %>% pull(label),
+    'C' = df %>% filter(phylogroup == 'C') %>% pull(label),
+    'G' = df %>% filter(phylogroup == 'G') %>% pull(label),
+    'F' = df %>% filter(phylogroup == 'F') %>% pull(label),
+    'E' = df %>% filter(phylogroup == 'E') %>% pull(label),
+    'D' = df %>% filter(phylogroup == 'D') %>% pull(label),
+    'E or cladeI' = df %>% filter(phylogroup == 'E or cladeI') %>% pull(label) ,
+    'cladeI' = df %>% filter(phylogroup == 'cladeI') %>% pull(label)
+  )
+  
+  tree = tree %>% groupOTU(grp, 'phylogroup')
+  return(tree)
+}
+
+tree_phylogroup(tree = tree)
+
 
 # 
 ggtree(tree, layout = 'fan', size = 0.4)+ aes(color = phylogroup) +
@@ -265,8 +305,8 @@ worm.img.dist = worm.img %>%
   left_join(metadata) %>%
   filter(Discard == 'No') %>% 
   mutate(Annotation_0mM = case_when(Annotation_0mM == 'normal' | Annotation_0mM == 'NB' ~ 0,
-                                 Annotation_0mM == 'biofilm' ~ 1,
-                                 Annotation_0mM == 'super_bio' | Annotation_0mM == 'super_biofilm' ~ 2),
+                                    Annotation_0mM == 'biofilm' ~ 1,
+                                    Annotation_0mM == 'super_bio' | Annotation_0mM == 'super_biofilm' ~ 2),
          Annotation_50mM = case_when(Annotation_50mM == 'normal' ~ 0,
                                      Annotation_50mM == 'biofilm' ~ 1,
                                      Annotation_50mM == 'super_bio' |Annotation_50mM == 'super_biofilm' ~ 2),
@@ -473,8 +513,8 @@ p3 = gheatmap(p3, distance, offset = 10.4,  width=.1,
                                 '#5963FA', # blue
                                 '#FF8D38', # orange 
                                 '#E64040'  # red
-                               ) 
-                    )
+                    ) 
+  )
 
 # 4. Pacs-2::GFP imaging 
 
@@ -498,6 +538,256 @@ rotate_tree(p5, 90)
 
 ggsave(file = here('exploration', 'rtree_phylog_biofilm.pdf'), width = 180, height = 180, units = 'mm', scale = 2, device = 'pdf')
 
+
+
+
+# Filipe's grant ----------------------------------------------------------
+
+# info = worm.img %>%
+#   select(ID:FDR_stars) %>%  # select only relevant categories
+#   left_join(metadata) %>%
+#   filter(Discard == 'No') %>%
+#   left_join(worm.img.dist %>%
+#               select(ID, distance)) %>%
+#   filter(!phylogroup %in% c('cladeI', 'cladeII', 'cladeIII', 'cladeIV', 'cladeV', 'E or cladeI', 'albertii')) %>%
+#   drop_na(phylogroup) %>%
+#   select(-Plate) %>%
+#   left_join(res2) %>%
+#   select(names, log2FC, FDR, distance, Mean = Bact_score_mean, Broadphenotype) %>%
+#   mutate(sig = case_when(FDR < 0.05 ~ '*',
+#                          FDR >= 0.05 ~ ''),
+#          distance = factor(distance, levels = c(-2,-1,0,1,2))) %>%
+#   select(-FDR) %>%
+#   mutate(Mean = case_when(Mean > 3 ~ 2.5,
+#                           Mean <= 20 ~ Mean)) %>%
+#   mutate(Broadphenotype = replace_na(Broadphenotype, 'Unknown')) %>%
+#   mutate(Broadphenotype = case_when(Broadphenotype == 'Commensalstrain' ~ 'Commensal strain',
+#                                     TRUE ~ Broadphenotype)) %>%
+#   distinct(names, .keep_all = T) %>%
+#   data.frame
+
+
+info = worm.img %>%
+  select(ID:FDR_stars) %>%  # select only relevant categories
+  left_join(metadata) %>%
+  filter(Discard == 'No') %>%
+  left_join(worm.img.dist %>%
+              select(ID, distance)) %>%
+  filter(!phylogroup %in% c('cladeI', 'cladeII', 'cladeIII', 'cladeIV', 'cladeV', 'E or cladeI', 'albertii')) %>%
+  drop_na(phylogroup) %>%
+  select(-Plate) %>%
+  left_join(res2 %>% select(ID, Bact_score_mean)) %>% 
+  mutate(Bact_score_mean = case_when(Bact_score_mean > 3 ~ 3,
+                                     TRUE ~ Bact_score_mean)) %>% 
+  mutate(extended_phenotype = case_when(is.na(extended_phenotype) ~ 'Unknown',
+                                        TRUE ~ extended_phenotype)) %>% 
+  # mutate(Broadphenotype = replace_na(Broadphenotype, 'Unknown')) %>%
+  # mutate(Broadphenotype = case_when(Broadphenotype == 'Commensalstrain' ~ 'Commensal strain',
+  #                                   TRUE ~ Broadphenotype)) %>% 
+  distinct(names, .keep_all = T) %>% 
+  data.frame
+
+
+rownames(info) = info$names
+info[,1] = NULL
+
+
+# df for log2FC
+logFC = data.frame(info$log2FC)
+rownames(logFC) = rownames(info)
+colnames(logFC) = c('Microbe-Drug-Host')
+
+# df for sig
+base = data.frame(info$Met_0mM)
+rownames(base) = rownames(info)
+colnames(base) = c('Microbe-Host')
+
+# resistance
+res3 = data.frame(info$Bact_score_mean)
+rownames(res3) = rownames(info)
+colnames(res3) = c('Microbial Growth')
+
+# phenotype
+pheno = data.frame(info$extended_phenotype)
+rownames(pheno) = rownames(info)
+colnames(pheno) = 'Microbial Ecological Niche'
+
+# # distance
+# distance = data.frame(info$distance)
+# rownames(distance) = rownames(info)
+# colnames(distance) = 'Phenotype distance'
+
+tree$tip.label[!(tree$tip.label %in% row.names(logFC))]
+
+to_drop = 
+  tree$tip.label[!(tree$tip.label %in% row.names(logFC))]
+
+tree_reduced = drop.tip(tree, to_drop)
+
+tree_reduced = tree_phylogroup(tree = tree_reduced)
+
+p = ggtree(tree_reduced, layout = 'fan', open.angle = 25, branch.length="none", size = 0.5, color = 'grey') +
+  geom_tiplab(size = 1.3, aes(color = phylogroup, angle = angle, show.legend = FALSE)) +
+  scale_colour_manual(values = c(
+    "#E41A1C", # A
+    "#377EB8", # B1
+    "#4DAF4A", # B2
+    "#984EA3", # C
+    # 'grey85',    # cladeI
+    "#FF7F00", # D
+    "#FFFF33", # E
+    # "#FFFF33", # E or clade I
+    "#A65628", # F
+    "#F781BF"  # G  
+  )) + guides(colour = guide_legend(override.aes = list(size = 6, pch = 18)))
+
+
+
+
+
+# 1. bacterial phenotype
+p1 = gheatmap(p, pheno, offset = 4, width=0.1,
+              colnames_angle = 0, hjust = -0.06, colnames_offset_x = 0.25, color=NA) +
+  scale_fill_manual(values = c('#1D6EDB',
+                               '#1CDB06',
+                               '#DB1D97',
+                               '#DB9612',
+                               'grey50'), name = "Bacterial Phenotype")
+
+# 2. bacterial resistance score
+p2 = p1 + new_scale_fill()
+
+p2 = gheatmap(p2, res3, offset = 7.2,  width=.1,
+              colnames_angle = 0, hjust = -0.2, colnames_offset_y = 0.25,color=NA) +
+  scale_fill_viridis_c(name = 'Bacterial score', direction = -1)
+
+
+# 3. Fold change of worm imaging
+p3 = p2 + new_scale_fill()
+
+p3 = gheatmap(p3, logFC, offset = 13.1,  width=.1,
+              colnames_angle = 0, hjust = -0.2, colnames_offset_y = 0.25,color=NA) +
+  scale_fill_viridis_c(option="D",direction = -1, name = 'Pacs2::GFP log2 Fold Change')
+
+# 4. GFP control values
+p4 = p3 + new_scale_fill()
+
+p4 = gheatmap(p4, base, offset = 10.1,  width=.1,
+              colnames_angle = 0, hjust = -0.2, colnames_offset_y = 0.25,color=NA) +
+  scale_fill_viridis_c(option="B", direction = -1, name = 'Pacs2::GFP Control')
+
+
+rotate_tree(p4, 90)
+
+
+ggsave(file = here('exploration', 'grant_tree.pdf'),
+       width = 200, height = 200, units = 'mm', scale = 2, 
+       device = 'pdf')
+
+
+
+
+# iTOL files --------------------------------------------------------------
+
+
+# let's create an iTOL annotation file to colour the labels in the tree
+
+library(readxl)
+metadata = read_excel("tree_files/MAIN_metadata.xlsx")
+
+
+
+### IMPORTANT
+
+strains = read_delim("tree_files/strains.tsv", 
+                      delim = "\t", escape_double = FALSE, 
+                      trim_ws = TRUE)
+
+
+
+strains = strains %>% 
+  mutate(extended_phenotype = 
+           case_when(Phenotype == 'Laboratory strain' ~ 'Laboratory strain',
+                     str_detect(`Isolation origin`, 'Human') ~ 'Human commensal',
+                     str_detect(`Isolation origin`, 'human') ~ 'Human commensal',
+                     Phenotype == 'Commensal strain' ~ 'Animal commensal',
+                     Phenotype == 'Pathogenic strain' ~ 'Pathogenic strain',
+                     is.na(Phenotype) ~ 'Unknown',
+                     Phenotype == 'Unknown' ~ 'Unknown',
+                     Phenotype == 'Evolution experiment' ~ 'Evolution experiment'),
+         .before = `Parental Strain`) 
+
+
+metadata %>% 
+  left_join(
+    strains %>% 
+      select(ID = `Strain Identifier`, extended_phenotype)
+  ) %>% 
+  mutate(extended_phenotype = 
+           case_when(is.na(extended_phenotype) ~ Broadphenotype,
+                     TRUE ~ extended_phenotype)) %>% 
+  mutate(extended_phenotype = str_replace_all(extended_phenotype, 
+                                              'Commensal strain',
+                                              'Human commensal'),
+         extended_phenotype = case_when(
+           Broadphenotype == 'Pathogenic strain' ~ 'Pathogenic strain',
+           TRUE ~ extended_phenotype)) %>% 
+  select(fasta:Broadphenotype, extended_phenotype, everything()) %>% 
+  write.xlsx('Metadata_phenotype_extended.xlsx')
+
+metadata = read_excel('Metadata_phenotype_extended.xlsx')
+
+# "#E41A1C", # A
+# "#377EB8", # B1
+# "#4DAF4A", # B2
+# "#984EA3", # C
+# 'E8E8E8',    # cladeI
+# "#FF7F00", # D
+# "#FFFF33", # E
+# "#FFFF33", # E or clade I
+# "#A65628", # F
+# "#F781BF"  # G  
+
+metadata %>% 
+  filter(Discard == 'No') %>% 
+  mutate(ID = str_replace_all(fasta, '.fasta', '')) %>% 
+  select(ID, phylogroup) %>% 
+  mutate(color = 
+           case_when(phylogroup == 'A' ~ '#E41A1C',
+                     phylogroup == 'B1' ~ '#377EB8',
+                     phylogroup == 'B2' ~ '#4DAF4A',
+                     phylogroup == 'C' ~ '#984EA3',
+                     phylogroup == 'cladeI' ~ '#E8E8E8',
+                     phylogroup == 'D' ~ '#FF7F00',
+                     phylogroup == 'E' ~ '#FFFF33',
+                     phylogroup == 'E or clade I' ~ '#FFFF33',
+                     phylogroup == 'F' ~ '#A65628',
+                     phylogroup == 'G' ~ '#F781BF',
+           )
+  ) %>% 
+  mutate(type = 'label', 
+         font = 'bold', 
+         size = 1) %>% 
+  select(ID, type, color, font, size) %>% 
+  write_delim('label_colours.txt')
+
+
+
+metadata %>% 
+  filter(Discard == 'No') %>% 
+  mutate(ID = str_replace_all(fasta, '.fasta', '')) %>% 
+  select(ID, extended_phenotype) %>% 
+  drop_na() %>% 
+  mutate(color = 
+           case_when(
+             extended_phenotype == 'Human commensal' ~ '#219FEB',
+             extended_phenotype == 'Animal commensal' ~ '#5CEB09',
+             extended_phenotype == 'Pathogenic strain' ~ '#EB9015',
+             extended_phenotype == 'Laboratory strain' ~ '#E821EB'
+           )) %>% 
+  select(ID, color, extended_phenotype) %>% 
+  write_delim('colorstrip_vars.txt', col_names = FALSE, quote = 'none')
+  
 
 
 
